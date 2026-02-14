10ed1a2732020fdfdb122316fa775c14d21c97ae
Vadlamudi Puneeth <nanipuneeth00@gmail.com>
2026-02-14 15:27:41 +0530

    small test

--- mockitowithouthibernate/src/test/java/com/mock/UserServiceTest.java
+++ mockitowithouthibernate/src/test/java/com/mock/UserServiceTest.java
@@ -5,19 +5,33 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+	@Mock
+	UserDao daoMock;
+	
+	@InjectMocks
+	UserService service;
+	
 	@Test
 	public void typeOfUser() {
-		UserDao daomock = mock(UserDao.class);
+//		UserDao daomock = mock(UserDao.class);
 		
 		User fakeObject = new User();
 		fakeObject.setId(1);
 		fakeObject.setName("puneeth");
 		fakeObject.setBalance(1000);
 		
-		when(daomock.findById(1)).thenReturn(fakeObject);
-		UserService service = new UserService(daomock);
+		//mention what object should return
+		when(daoMock.findById(1)).thenReturn(fakeObject);
+		
+		//injecting mock
+//		service = new UserService(daoMock);
 		String res = service.typeOfUser(1);
 		
 		assertEquals("premium user", res);
