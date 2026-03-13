6b7a6a32eda257ac3a048b8ac5ebcaba02151891
Vadlamudi Puneeth <nanipuneeth00@gmail.com>
2026-03-13 12:22:30 +0530

    only one time user can request and store in Rabbitmq

--- MICROSERVICES/RABBITMQ/recharge_service/src/main/java/com/example/service/serv/RechargeService.java
+++ MICROSERVICES/RABBITMQ/recharge_service/src/main/java/com/example/service/serv/RechargeService.java
@@ -1,5 +1,8 @@
 package com.example.service.serv;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +14,24 @@ public class RechargeService {
 
 	private final RabbitTemplate rabbitTemplate;
 	
+	Set<String> set = new HashSet<>();
+	
 	public RechargeService(RabbitTemplate rabbitTemplate) {
 		this.rabbitTemplate = rabbitTemplate;
 	}
 	
 	public String sendRechargeRequest(RechargeProducerDTO dto) {
-		rabbitTemplate.convertAndSend(RabbitConfig.Queue_Name, dto);
-		return "message has been sent to broker";
+		if(!set.contains(dto.getMobileNumber())) {			
+			set.add(dto.getMobileNumber());
+			rabbitTemplate.convertAndSend(RabbitConfig.Queue_Name, dto);
+			return "message has been sent to broker";
+		}
+		
+		else {
+			return "only one time you can request";
+		}
+		
+		
 	}
 	
 	
